573d6dc78cf1c6803e71c9552cba18680991bffc
SGiuri <s.giuri@issm.it>
2021-03-26 13:46:38 +0100

    Initial commit

---
+++ ocr_numbers.py
@@ -0,0 +1,14 @@
+def convert(input_grid):
+
+    pass
+
+def convert_single_number(input_grid_3x4):
+
+    pass
+
+
+
+
+
+print(convert([" _ ", "| |", "|_|", "   "]))
+print(convert(["   ", "  |", "  |", "   "]))
